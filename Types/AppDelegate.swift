cecd9e6e209ba9049af1483b75d420a5c362dad4
Andrew Clissold <andrewclissold@gmail.com>
2015-07-25 11:28:10 -0400

    Port AppDelegate to Swift

---
+++ Types/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Types
+//
+//  Created by Andrew Clissold on 7/25/15.
+//  Copyright (c) 2015 Andrew Clissold. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
